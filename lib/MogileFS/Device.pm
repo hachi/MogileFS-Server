9901bb422d9ea3ad8c9b828ae4bcb2168a19d156
bradfitz <bradfitz@f67b2e87-0811-0410-a7e0-dd94e48410d6>
2006-11-14 23:18:02 +0000

    push directory vivification into the MogileFS::Device class, and make it smarter, remembering which hosts can't do MKCOL requests.
    git-svn-id: http://code.sixapart.com/svn/mogilefs/trunk@477 f67b2e87-0811-0410-a7e0-dd94e48410d6

--- lib/MogileFS/Device.pm
+++ lib/MogileFS/Device.pm
@@ -2,10 +2,13 @@ package MogileFS::Device;
 use strict;
 use warnings;
 
+my %singleton;  # devid -> instance
+
 sub of_devid {
     my ($class, $devid) = @_;
-    return bless {
-        devid => $devid,
+    return $singleton{$devid} ||= bless {
+        devid    => $devid,
+        no_mkcol => 0,
     }, $class;
 }
 
@@ -52,6 +55,54 @@ sub is_observed_writeable {
     return
         MogileFS->observed_state("host", $self->hostid) eq "reachable" &&
         MogileFS->observed_state("device", $self->{devid}) eq "writeable";
+}
+
+sub doesnt_know_mkcol {
+    my $self = shift;
+    # TODO: forget this periodically?  maybe whenever host/device is observed down?
+    # in case webserver changes.
+    return $self->{no_mkcol};
+}
+
+my %dir_made;  # /dev<n>/path -> $time
+my $dir_made_lastclean = 0;
+sub create_directory {
+    my ($self, $uri) = @_;
+    return 1 if $self->doesnt_know_mkcol;
+    next if $dir_made{$uri};
+
+    my $hostid = $self->hostid;
+    my $host = Mgd::hostid_ip($hostid)         or return 0;
+    my $port = Mgd::hostid_http_port($hostid)  or return 0;
+    my $peer = "$host:$port";
+
+    my $sock = IO::Socket::INET->new(PeerAddr => $peer, Timeout => 1)
+        or next;
+    print $sock "MKCOL $uri HTTP/1.0\r\n".
+        "Content-Length: 0\r\n\r\n";
+
+    my $ans = <$sock>;
+
+    # if they don't support this method, remember that
+    if ($ans && $ans =~ m!HTTP/1\.[01] (400|405|501)!) {
+        $self->{no_mkcol} = 1;
+        warn "NO MKCOL on $self->{devid} ....\n";
+        return 1;
+    }
+
+    return 0 unless $ans =~ m!^HTTP/1.[01] 2\d\d!;
+
+    my $now = time();
+    $dir_made{$uri} = $now;
+
+    # cleanup %dir_made occasionally.
+    my $clean_interval = 300;  # every 5 minutes.
+    if ($dir_made_lastclean < $now - $clean_interval) {
+        $dir_made_lastclean = $now;
+        foreach my $k (keys %dir_made) {
+            delete $dir_made{$k} if $dir_made{$k} < $now - 3600;
+        }
+    }
 }
 
 
