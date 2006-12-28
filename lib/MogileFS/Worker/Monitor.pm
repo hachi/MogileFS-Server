46848ab00d3e7acda98b49589ad8c1f91c1b5b93
bradfitz <bradfitz@f67b2e87-0811-0410-a7e0-dd94e48410d6>
2006-12-28 01:32:07 +0000

    update the monitor worker to be cleaner, smaller, and use the object-oriented apis.
    git-svn-id: http://code.sixapart.com/svn/mogilefs/trunk@529 f67b2e87-0811-0410-a7e0-dd94e48410d6

--- lib/MogileFS/Worker/Monitor.pm
+++ lib/MogileFS/Worker/Monitor.pm
@@ -2,15 +2,25 @@ package MogileFS::Worker::Monitor;
 
 use strict;
 use base 'MogileFS::Worker';
+use fields (
+            'last_db_update',  # devid -> time.  update db less often than poll interval.
+            'last_test_write', # devid -> time.  time we last tried writing to a device.
+            'skip_host',       # hostid -> 1 if already noted dead (reset every loop)
+            'seen_hosts',      # IP -> 1 (reset every loop)
+            );
 use Danga::Socket 1.56;
-use MogileFS::Util qw(error);
+use MogileFS::Util qw(error debug);
 use MogileFS::IOStatWatcher;
+
+use constant UPDATE_DB_EVERY => 15;
 
 sub new {
     my ($class, $psock) = @_;
     my $self = fields::new($class);
     $self->SUPER::new($psock);
 
+    $self->{last_db_update}  = {};
+    $self->{last_test_write} = {};
     return $self;
 }
 
@@ -21,150 +31,141 @@ sub watchdog_timeout {
 sub work {
     my $self = shift;
 
-    my $update_db_every = 15;
-    my %last_db_update;  # devid -> time.  update db less often than poll interval.
-    my %last_test_write; # devid -> time.  time we last tried writing to a device.
-
+    # we just forked from our parent process, also using Danga::Socket,
+    # so we need to lose all that state and start afresh.
     Danga::Socket->Reset;
 
     my $iow = MogileFS::IOStatWatcher->new;
     $iow->on_stats(sub {
         my ($host, $stats) = @_;
-#        use Data::Dumper;
-#        warn "IOWatcher: [$host] " . Dumper($stats) . "\n";
+        # use Data::Dumper;
+        # warn "IOWatcher: [$host] " . Dumper($stats) . "\n";
     });
 
-    # Create a closure that contains ourself
     my $main_monitor;
     $main_monitor = sub {
         Danga::Socket->AddTimer(2.5, $main_monitor);
         $self->parent_ping;
 
         # get db and note we're starting a run
-        error("Monitor running; scanning usage files")
-            if $Mgd::DEBUG >= 1;
+        debug("Monitor running; scanning usage files");
         $self->validate_dbh;
-        my $dbh = $self->get_dbh or return 0;
 
-        # get a current list of devices
-        my $devs = Mgd::get_device_summary();
-        unless ($devs && %$devs) {
-            $self->send_to_parent(":monitor_just_ran");
-            return;
-        }
+        $self->{skip_host}  = {};  # hostid -> 1 if already noted dead.
+        $self->{seen_hosts} = {}; # IP -> 1
 
         # now iterate over devices
-        my %skip_host;  # hostid -> 1 if already noted dead.
-        my %seen_hosts; # IP -> 1
-        foreach my $dev (values %$devs) {
-            next if $dev->{status} =~ /^dead|down$/;
-            next if $skip_host{$dev->{hostid}};
-
-            my $host = $Mgd::cache_host{$dev->{hostid}};
-            my $port = $host->{http_port};
-            my $get_port = $host->{http_get_port} || $port;
-            my $url = "http://$host->{hostip}:$get_port/dev$dev->{devid}/usage";
-            $seen_hosts{$host->{hostip}} = 1;
-
-            # now try to get the data with a short timeout
-            my $timeout = 2;
-            my $start_time = Time::HiRes::time();
-
-            my $ua = LWP::UserAgent->new( timeout => 2 );
-            my $response = $ua->get($url);
-            my $res_time = Time::HiRes::time();
-
-            unless ($response->is_success) {
-                my $failed_after = $res_time - $start_time;
-                if ($failed_after < 0.5) {
-                    $self->broadcast_device_unreachable($dev->{devid});
-                    error("Port $get_port not listening on otherwise-alive machine $host->{hostip}?  Error was: " . $response->status_line);
-                } else {
-                    $failed_after = sprintf("%.02f", $failed_after);
-                    $self->broadcast_host_unreachable($dev->{hostid});
-                    $skip_host{$dev->{hostid}} = 1;
-                    error("Timeout contacting machine $host->{hostip} for dev $dev->{devid}:  took $failed_after seconds out of $timeout allowed");
-                }
-                next;
-            }
-
-            # at this point we can reach the host
-            $self->broadcast_host_reachable($dev->{hostid});
-
-            my %stats;
-            my $data = $response->content;
-            foreach (split(/\r?\n/, $data)) {
-                next unless /^(\w+)\s*:\s*(.+)$/;
-                $stats{$1} = $2;
-            }
-
-            my ($used, $total) = ($stats{used}, $stats{total});
-            unless ($used && $total) {
-                error("dev$dev->{devid} reports used = $used, total = $total, error?");
-                next;
-            }
-
-            # only update database every ~15 seconds per device
-            my $last_update = $last_db_update{$dev->{devid}} || 0;
-            my $next_update = $last_update + $update_db_every;
-            my $now = time();
-            if ($now >= $next_update) {
-                $dbh->do("UPDATE device SET mb_total = ?, mb_used = ?, mb_asof = UNIX_TIMESTAMP() " .
-                         "WHERE devid = ?", undef, int($total / 1024), int($used / 1024), $dev->{devid});
-                if ($dbh->err) {
-                    error("Database error in update query: " . $dbh->errstr);
-                    next;
-                }
-                $last_db_update{$dev->{devid}} = $now;
-            }
-
-            # next if we're not going to try this now
-            next if $last_test_write{$dev->{devid}} + $update_db_every > $now;
-            $last_test_write{$dev->{devid}} = $now;
-
-            # now we want to check if this device is writeable
-            my $num = int(rand 10000);  # this was "$$-$now" before, but we don't yet have a cleaner in mogstored for these files
-            my $puturl = "http://$host->{hostip}:$port/dev$dev->{devid}/test-write/test-write-$num";
-            my $content = "time=$now rand=$num";
-            my $req = HTTP::Request->new(PUT => $puturl);
-            $req->content($content);
-
-            # TODO: guard against race-conditions with double-check on failure
-
-            # now, depending on what happens
-            my $resp = $ua->request($req);
-            if ($resp->is_success) {
-                # now let's get it back to verify; note we use the get_port to verify that
-                # the distinction works (if we have one)
-                my $geturl = "http://$host->{hostip}:$get_port/dev$dev->{devid}/test-write/test-write-$num";
-                my $testwrite = $ua->get($geturl);
-
-                # if success and the content matches, mark it writeable
-                if ($testwrite->is_success && $testwrite->content eq $content) {
-                    $self->broadcast_device_writeable($dev->{devid});
-                    error("dev$dev->{devid}: used = $used, total = $total, writeable = 1")
-                        if $Mgd::DEBUG >= 1;
-                    next;
-                }
-
-            }
-
-            # if we fall through to here, then we know that something is not so good, so mark it readable
-            # which is guaranteed given we even tested writeability
-            $self->broadcast_device_readable($dev->{devid});
-            error("dev$dev->{devid}: used = $used, total = $total, writeable = 0")
-                if $Mgd::DEBUG >= 1;
+        foreach my $dev (MogileFS::Device->devices) {
+            next if $dev->is_marked_dead || $dev->is_marked_down;
+            next if $self->{skip_host}{$dev->hostid};
+            $self->check_device($dev);
         }
 
-        $iow->set_hosts(keys %seen_hosts);
-
-        # announce to the parent that we've run
+        $iow->set_hosts(keys %{$self->{seen_hosts}});
         $self->send_to_parent(":monitor_just_ran");
     };
 
     $main_monitor->();
-
     Danga::Socket->EventLoop;
+}
+
+sub check_device {
+    my ($self, $dev) = @_;
+
+    my $devid = $dev->id;
+
+    my $host = $Mgd::cache_host{$dev->hostid};
+    my $port = $host->{http_port};
+    my $get_port = $host->{http_get_port} || $port;
+    my $url = "http://$host->{hostip}:$get_port/dev$devid/usage";
+    $self->{seen_hosts}{$host->{hostip}} = 1;
+
+    # now try to get the data with a short timeout
+    my $timeout = 2;
+    my $start_time = Time::HiRes::time();
+
+    my $ua = LWP::UserAgent->new( timeout => 2 );
+    my $response = $ua->get($url);
+    my $res_time = Time::HiRes::time();
+
+    unless ($response->is_success) {
+        my $failed_after = $res_time - $start_time;
+        if ($failed_after < 0.5) {
+            $self->broadcast_device_unreachable($dev->id);
+            error("Port $get_port not listening on otherwise-alive machine $host->{hostip}?  Error was: " . $response->status_line);
+        } else {
+            $failed_after = sprintf("%.02f", $failed_after);
+            $self->broadcast_host_unreachable($dev->hostid);
+            $self->{skip_host}{$dev->hostid} = 1;
+            error("Timeout contacting machine $host->{hostip} for dev $devid:  took $failed_after seconds out of $timeout allowed");
+        }
+        return;
+    }
+
+    # at this point we can reach the host
+    $self->broadcast_host_reachable($dev->hostid);
+
+    my %stats;
+    my $data = $response->content;
+    foreach (split(/\r?\n/, $data)) {
+        next unless /^(\w+)\s*:\s*(.+)$/;
+        $stats{$1} = $2;
+    }
+
+    my ($used, $total) = ($stats{used}, $stats{total});
+    unless ($used && $total) {
+        error("dev$devid reports used = $used, total = $total, error?");
+        return;
+    }
+
+    # only update database every ~15 seconds per device
+    my $last_update = $self->{last_db_update}{$dev->id} || 0;
+    my $next_update = $last_update + UPDATE_DB_EVERY;
+    my $now = time();
+    if ($now >= $next_update) {
+        my $dbh = $self->get_dbh or return 0;
+        $dbh->do("UPDATE device SET mb_total = ?, mb_used = ?, mb_asof = UNIX_TIMESTAMP() " .
+                 "WHERE devid = ?", undef, int($total / 1024), int($used / 1024), $devid);
+        if ($dbh->err) {
+            error("Database error in update query: " . $dbh->errstr);
+            return;
+        }
+        $self->{last_db_update}{$devid} = $now;
+    }
+
+    # next if we're not going to try this now
+    return if $self->{last_test_write}{$devid} + UPDATE_DB_EVERY > $now;
+    $self->{last_test_write}{$devid} = $now;
+
+    # now we want to check if this device is writeable
+    my $num = int(rand 10000);  # this was "$$-$now" before, but we don't yet have a cleaner in mogstored for these files
+    my $puturl = "http://$host->{hostip}:$port/dev$devid/test-write/test-write-$num";
+    my $content = "time=$now rand=$num";
+    my $req = HTTP::Request->new(PUT => $puturl);
+    $req->content($content);
+
+    # TODO: guard against race-conditions with double-check on failure
+
+    # now, depending on what happens
+    my $resp = $ua->request($req);
+    if ($resp->is_success) {
+        # now let's get it back to verify; note we use the get_port to verify that
+        # the distinction works (if we have one)
+        my $geturl = "http://$host->{hostip}:$get_port/dev$devid/test-write/test-write-$num";
+        my $testwrite = $ua->get($geturl);
+
+        # if success and the content matches, mark it writeable
+        if ($testwrite->is_success && $testwrite->content eq $content) {
+            $self->broadcast_device_writeable($devid);
+            debug("dev$devid: used = $used, total = $total, writeable = 1");
+            return;
+        }
+    }
+
+    # if we fall through to here, then we know that something is not so good, so mark it readable
+    # which is guaranteed given we even tested writeability
+    $self->broadcast_device_readable($devid);
+    debug("dev$devid: used = $used, total = $total, writeable = 0");
 }
 
 1;
