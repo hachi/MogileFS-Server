ed5963615cd4d198d7de36661cf5f92d6c63bc6f
bradfitz <bradfitz@f67b2e87-0811-0410-a7e0-dd94e48410d6>
2006-12-29 08:48:47 +0000

    doc fixes
    git-svn-id: http://code.sixapart.com/svn/mogilefs/trunk@587 f67b2e87-0811-0410-a7e0-dd94e48410d6

--- makedocs.pl
+++ makedocs.pl
@@ -6,7 +6,8 @@ push @files, `find lib -name '*.pm'`;
 chomp @files;
 
 my $base = "/home/lj/htdocs/dev/mogdocs/";
-system("find $base -type f -exec rm {} \;");
+#system("find $base -type f -exec rm {} \;");
+my %html; # perl file -> html file
 
 foreach my $f (@files) {
     my $outfile = $f;
@@ -14,6 +15,28 @@ foreach my $f (@files) {
     $outfile =~ s!\.pm$!!;
     $outfile .= ".html";
     system("install -D /dev/null $base/$outfile");
-    system("pod2html --htmlroot=/dev/mogdocs $f > $base/$outfile");
+    system("pod2html --podroot=. --podpath=.:lib --htmlroot=/dev/mogdocs $f > $base/$outfile");
     print "F = $f => $outfile\n";
+    $html{$f} = $outfile;
+}
+
+open (my $fh, ">$base/index.html") or die;
+print $fh "<html><head><title>MogileFS server docs</title></head><body><pre>";
+foreach my $f (@files) {
+    if (has_pod($f)) {
+        print $fh "<a href='$html{$f}'>$f</a>\n";
+    } else {
+        print $fh "$f\n";
+    }
+}
+print $fh "</pre></body></html>\n";
+close($fh);
+
+sub has_pod {
+    my $f = shift;
+    open(my $fh, $f) or die;
+    while (<$fh>) {
+        return 1 if /=head1/;
+    }
+    return 0;
 }
