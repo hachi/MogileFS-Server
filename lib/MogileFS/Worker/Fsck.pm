b269d1e66e9ec21ce4bd8191e960d04d4ef7464d
bradfitz <bradfitz@f67b2e87-0811-0410-a7e0-dd94e48410d6>
2006-12-29 04:55:09 +0000

    remove unused stuff.  this whole file is unused.  what is diff between this and Checker.pm?
    git-svn-id: http://code.sixapart.com/svn/mogilefs/trunk@574 f67b2e87-0811-0410-a7e0-dd94e48410d6

--- lib/MogileFS/Worker/Fsck.pm
+++ lib/MogileFS/Worker/Fsck.pm
@@ -11,17 +11,12 @@ sub watchdog_timeout {
 sub work {
     my $self = shift;
 
-    my $update_db_every = 15;
-    my %last_db_update;  # devid -> time.  update db less often than poll interval.
-
     every(10, sub {
         $self->parent_ping;
 
         # get db and note we're starting a run
         error("Monitor running; scanning usage files")
             if $Mgd::DEBUG >= 1;
-        $self->validate_dbh;
-        my $dbh = $self->get_dbh or return 0;
 
 
 
