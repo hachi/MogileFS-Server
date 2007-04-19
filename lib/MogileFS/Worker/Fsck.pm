894558e2dc38e42bb610492222ad3641b7d2d8ff
bradfitz <bradfitz@f67b2e87-0811-0410-a7e0-dd94e48410d6>
2007-04-19 00:38:10 +0000

    fsck worker is starting to take shape into what is almost a workable form.
    git-svn-id: http://code.sixapart.com/svn/mogilefs/trunk@851 f67b2e87-0811-0410-a7e0-dd94e48410d6

--- lib/MogileFS/Worker/Fsck.pm
+++ lib/MogileFS/Worker/Fsck.pm
@@ -91,24 +91,22 @@ sub check_fid {
     my $opt_no_stat = delete $opts{no_stat};
     die "badopts" if %opts;
 
+    # first obvious fucked-up case:  no devids even presumed to exist.
+    unless (@{ $fid->devids }) {
+        # first, log this weird condition. (N = NO PAths)
+        Mgd::get_store->fsck_log(code => "NOPA", fid => $fid->id);
+        # weird, schedule a fix (which will do a search over all
+        # devices as a last-ditch effort to locate it)
+        $self->fix_fid($fid);
+        return 1;
+    }
+
     # first, see if the assumed devids meet the replication policy for
     # the fid's class.
     unless ($fid->devids_meet_policy) {
-        printf("FID %d (len=%d; cldid=%d) doesn't meet repl policy (on: %s)\n",
-               $fid->id,
-               $fid->length,
-               $fid->classid,
-               join(",", $fid->devids),
-               );
-
-        # TODO: does this do it immediately, or later? should decide
-        # on whether there's 1 copy or more.  or do we check number of
-        # good copies first, in this case?:
-        $fid->enqueue_for_replication;
-
-        # TODO: log to fsck_log
-        # ....
-
+        # log a policy violation
+        Mgd::get_store->fsck_log(code => "POVI", fid => $fid->id);
+        $self->fix_fid($fid);
         return 1;
     }
 
@@ -116,7 +114,6 @@ sub check_fid {
     # locations are actually there).  in the fast case, all we do is
     # check the replication policy, which is already done, so finish.
     return 1 if $opt_no_stat;
-
 
     # **********************************************************************
     # FIXME: temporary, until statting is done...
@@ -163,6 +160,80 @@ sub check_fid {
                 return 0;
             }
         }
+    }
+
+    return 1;
+}
+
+# this is the slow path.  if something above in check_fid finds
+# something amiss in any way, we went the slow path on a fid and try
+# really hard to fix the situation.
+#
+# die on errors.  return (anything) on success.
+sub fix_fid {
+    my ($self, $fid) = @_;
+
+    printf("Fixing FID %d (len=%d; cldid=%d; on: %s)\n",
+           $fid->id,
+           $fid->length,
+           $fid->classid,
+           join(",", $fid->devids),
+           );
+
+    # make devfid objects from the devids that this fid is on,
+    my @dfids = map { MogileFS::DevID->new($_, $fid) } @{ $fid->devids };
+
+    # keep track if we found the file (with the right size) at least somewhere.
+    # value is 0 if not, or a devid that has the correct length.
+    my $found_it = 0;
+
+    my $check_dfids = sub {
+        my $stop_on_found = shift;
+
+        # stat all devices.
+        foreach my $dfid (@dfids) {
+            # good case.
+            if ($dfid->size_matches) {
+                $found_it ||= $dfid->devid;
+                return if $stop_on_found;
+                next;
+            }
+
+            # bad case:
+            # FIXME: log difference between missing & wrong size.  wrong size might be better
+            # than nothing.
+            # ALSO:  make sure we very well tell difference between host down/timeout vs. up & file gone
+            # TODO: if really, really gone, remove it from devid table...
+        }
+    };
+
+    $check_dfids->();
+
+    # if we didn't find it anywhere, let's go do an exhaustive search over
+    # all devices, looking for it...
+    unless ($found_it) {
+        # TODO: replace @dfids with list of all (alive) devices which weren't previously
+        # searched,
+
+        # then check again:
+        $check_dfids->("stop_on_found");
+        if (! $found_it) {
+            # shit, really fucked.  where'd it go?  log it and proceed, sadly.
+            return 1;
+        }
+
+        # TODO: log that crazy search found it.  yay!
+        # then fall through to below to check policy on if it's replicated enough....
+    }
+
+    # If we messed with a Dev's devices, make sure our dev object
+    # isn't caching the devids.  so let's just wipe it and reload it:
+    $fid->forget_cached_devids;  # TODO
+
+    unless ($fid->devids_meet_policy) {
+        $fid->enqueue_for_replication;
+        # TODO: log that we enqueud it for replication
+        return 1;
     }
 
     return 1;
