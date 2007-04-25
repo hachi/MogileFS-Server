fb953d6009a3d26a5d4232d68ba0f2bae1fb789d
bradfitz <bradfitz@f67b2e87-0811-0410-a7e0-dd94e48410d6>
2007-04-25 18:48:23 +0000

    lighttpd support
    git-svn-id: http://code.sixapart.com/svn/mogilefs/trunk@912 f67b2e87-0811-0410-a7e0-dd94e48410d6

--- lib/Mogstored/HTTPServer.pm
+++ lib/Mogstored/HTTPServer.pm
@@ -6,6 +6,7 @@ sub new {
     $self->{docroot}  = delete $opts{docroot};
     $self->{listen}   = delete $opts{listen};
     $self->{maxconns} = delete $opts{maxconns};
+    $self->{bin}      = delete $opts{bin};
     die "unknown opts" if %opts;
     return $self;
 }
@@ -23,5 +24,12 @@ sub post_daemonize {
     my $self = shift;
 }
 
+sub listen_port {
+    my $self = shift;
+    my $port = $self->{listen};
+    $port =~ s/^.+://;
+    die "not numeric port?" unless $port =~ /^\d+$/;
+    return $port;
+}
 
 1;
--- lib/Mogstored/HTTPServer/Lighttpd.pm
+++ lib/Mogstored/HTTPServer/Lighttpd.pm
@@ -1,9 +1,58 @@
 package Mogstored::HTTPServer::Lighttpd;
 use strict;
 use base 'Mogstored::HTTPServer';
+use File::Temp ();
 
 sub start {
-    die "TODO: start lighttpd";
+    my $self = shift;
+    my $exe = $self->{bin};
+
+    if ($exe && -x $exe) {
+        die "Provided lighttpd path $exe not valid.\n";
+    }
+    unless ($exe) {
+        my @loc = qw(/usr/local/sbin/lighttpd
+                     /usr/sbin/lighttpd
+                     /usr/local/bin/lighttpd
+                     /usr/bin/lighttpd
+                     );
+        foreach my $loc (@loc) {
+            $exe = $loc;
+            last if -x $exe;
+        }
+        unless (-x $exe) {
+            die "Can't find lighttpd in @loc\n";
+        }
+    }
+
+    my $pid = fork();
+    die "Can't fork: $!" unless defined $pid;
+
+    if ($pid) {
+        $self->{pid} = $pid;
+        Mogstored->on_pid_death($pid => sub {
+            die "lighttpd died";
+        });
+        return;
+    }
+
+    my ($fh, $filename) = File::Temp::tempfile();
+    $self->{temp_conf_file} = $filename;
+    my $portnum = $self->listen_port;
+
+    print $fh qq{
+server.document-root = "$self->{docroot}"
+server.port = $portnum
+server.modules = ( "mod_webdav" )
+webdav.activate = "enable"
+};
+
+    exec $exe, "-D", "-f", $filename;
+}
+
+sub DESTROY {
+    my $self = shift;
+    unlink $self->{temp_conf_file} if $self->{temp_conf_file};
 }
 
 1;
