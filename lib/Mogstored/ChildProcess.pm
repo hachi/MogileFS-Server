a98cc028ad18d9123105de80b4ec40165ddf5a00
bradfitz <bradfitz@f67b2e87-0811-0410-a7e0-dd94e48410d6>
2007-05-09 19:05:51 +0000

    PAR work
    git-svn-id: http://code.sixapart.com/svn/mogilefs/trunk@980 f67b2e87-0811-0410-a7e0-dd94e48410d6

--- lib/Mogstored/ChildProcess.pm
+++ lib/Mogstored/ChildProcess.pm
@@ -13,7 +13,21 @@ sub pre_exec_init {
 
 sub exec {
     my $class = shift;
-    exec $^X, "-M$class", "-e", "$class->run;";
+    if (_running_under_par()) {
+        # then we can't exec, as we'll lose magic @INC
+        # ghetto:
+        #for (3..100) { POSIX::close($_); }
+        my $rv = eval "use $class; 1" or die "Failed to load $class: $@\n";
+        $class->run;
+    } else {
+        exec $^X, "-M$class", "-e", "$class->run;";
+    }
+    die "$class run loop ended!\n";
+}
+
+sub _running_under_par {
+    # not the best test in the world, but works.
+    return (grep { ref $_ eq "CODE" } @INC) ? 1 : 0;
 }
 
 1;
