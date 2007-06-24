hachi/MogileFS-Server

commit 883ec5782cd1767780b5c3bb17364f95bdc97fd2

Fix deletion crash if a file existed in the database but was not on any device. INNER JOIN instead of LEFT JOIN.
Signed-off-by: Robin H. Johnson <robbat2@gentoo.org>


git-svn-id: http://code.sixapart.com/svn/mogilefs/trunk@1033 f67b2e87-0811-0410-a7e0-dd94e48410d6

lib/MogileFS/Worker/Delete.pm
@@ -125,7 +125,7 @@ sub process_deletes {
 
     my $delmap = $dbh->selectall_arrayref("SELECT fd.fid, fo.devid ".
                                           "FROM file_to_delete fd ".
-                                          "LEFT JOIN file_on fo ON fd.fid=fo.fid ".
+                                          "INNER JOIN file_on fo ON fd.fid=fo.fid ".
                                           "LIMIT " . LIMIT);
     my $count = $delmap ? scalar @$delmap : 0;
     return 0 unless $count;

commit 2f89d1d3aea3254218b68bc791fac4615f44c3c9

Add a testcase that catches the bug that would have been introduced by r1033, while further debugging takes place.
Signed-off-by: Robin H. Johnson <robbat2@gentoo.org>


git-svn-id: http://code.sixapart.com/svn/mogilefs/trunk@1038 f67b2e87-0811-0410-a7e0-dd94e48410d6

t/00-startup.t
@@ -20,7 +20,7 @@ find_mogclient_or_skip();
 
 my $sto = eval { temp_store(); };
 if ($sto) {
-    plan tests => 56;
+    plan tests => 59;
 } else {
     plan skip_all => "Can't create temporary test database: $@";
     exit 0;
@@ -65,9 +65,9 @@ my $be = $mogc->{backend}; # gross, reaching inside of MogileFS::Client
 my $lasttime = 1167609600; # Mon Jan  1 00:00:00 UTC 2007
 ok(try_for(3, sub {
     my $timestamp = $dbh->selectrow_array("SELECT ".$sto->unix_timestamp);
-	# FIXME: Some databases might be pedantic about the FROM
-	# but having it on others means that if the table has no rows
-	# we won't get any results!
+    # FIXME: Some databases might be pedantic about the FROM
+    # but having it on others means that if the table has no rows
+    # we won't get any results!
     my $rv = $timestamp > $lasttime;
     $lasttime = $timestamp;
     return $rv;
@@ -248,6 +248,14 @@ ok(try_for(20, sub {
     return @files == 0;
 }), "and they're gone from filesystem");
 
+foreach my $t (qw(file file_on file_to_delete)) {
+    my $rows = $dbh->selectrow_array("SELECT COUNT(*) FROM $t");
+	if($rows > 0) {
+		fail("$rows files still exist in $t!")
+	} else {
+		pass("$t cleaned");
+	}
+}
 
 sub try_for {
     my ($tries, $code) = @_;