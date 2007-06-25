69175545767dc40018a811a7210be1d70092966c
bradfitz <bradfitz@f67b2e87-0811-0410-a7e0-dd94e48410d6>
2007-06-25 21:53:04 +0000

    start of support to make mogstored wrap Apache + web_dav
    git-svn-id: http://code.sixapart.com/svn/mogilefs/trunk@1057 f67b2e87-0811-0410-a7e0-dd94e48410d6

--- lib/Mogstored/HTTPServer/Apache.pm
+++ lib/Mogstored/HTTPServer/Apache.pm
@@ -1,4 +1,4 @@
-package Mogstored::HTTPServer::Lighttpd;
+package Mogstored::HTTPServer::Apache;
 use strict;
 use base 'Mogstored::HTTPServer';
 use File::Temp ();
@@ -8,20 +8,19 @@ sub start {
     my $exe = $self->{bin};
 
     if ($exe && ! -x $exe) {
-        die "Provided lighttpd path $exe not valid.\n";
+        die "Provided apache path $exe not valid.\n";
     }
     unless ($exe) {
-        my @loc = qw(/usr/local/sbin/lighttpd
-                     /usr/sbin/lighttpd
-                     /usr/local/bin/lighttpd
-                     /usr/bin/lighttpd
+        # TODO: not sure where else common locations are... just guessing
+        my @loc = qw(/usr/sbin/apache
+                     /usr/sbin/httpd
                      );
         foreach my $loc (@loc) {
             $exe = $loc;
             last if -x $exe;
         }
         unless (-x $exe) {
-            die "Can't find lighttpd in @loc\n";
+            die "Can't find apache in @loc\n";
         }
     }
 
@@ -31,7 +30,7 @@ sub start {
     if ($pid) {
         $self->{pid} = $pid;
         Mogstored->on_pid_death($pid => sub {
-            die "lighttpd died";
+            die "apache died";
         });
         return;
     }
@@ -43,15 +42,26 @@ sub start {
     my $bind_ip = $self->bind_ip;
 
     print $fh qq{
-server.document-root = "$self->{docroot}"
-server.port = $portnum
-server.bind = "$bind_ip"
-server.modules = ( "mod_webdav", "mod_status" )
-webdav.activate = "enable"
-status.status-url  = "/"
+ServerType standalone
+ErrorLog /dev/null
+LoadModule dav_module  /usr/lib/apache/1.3/libdav.so
+
+Listen 7500
+<VirtualHost *:7500>
+  DocumentRoot $self->{docroot}
+
+  <Directory $self->{docroot}>
+    Options +Indexes +FollowSymLinks
+  </Directory>
+
+  <Location />
+    DAV On
+  </Location>
+</VirtualHost>
+
 };
 
-    exec $exe, "-D", "-f", $filename;
+    exec $exe, "-F", "-f", $filename;
 }
 
 sub DESTROY {
