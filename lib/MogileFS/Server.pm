edbffce5a8d4ffc48d05707da0ba018d456ff6c5
bradfitz <bradfitz@f67b2e87-0811-0410-a7e0-dd94e48410d6>
2007-07-03 21:02:27 +0000

    make mogstored and mogilefsd use the now-bundled dependency Perl modules as a last resort, unless MOGILE_NO_BUILTIN_DEPS environment variable is true.  this makes it easier for people to use mogilefs, without dependency hell.  distros/packagers are expected to ignore (or scoff at) this "feature".  but easier for people working from tarball/svn releases.  bundled dependencies will be updated over time, as needed.
    git-svn-id: http://code.sixapart.com/svn/mogilefs/trunk@1088 f67b2e87-0811-0410-a7e0-dd94e48410d6

--- lib/MogileFS/Server.pm
+++ lib/MogileFS/Server.pm
@@ -15,6 +15,25 @@ MogileFS::Server - MogileFS (distributed filesystem) server
 
 =cut
 
+# based on where we found this file (a pure-perl module),
+# add the mogdeps/ subdirectory of that base to our @INC search
+# path, where all the misc Mogile dependencies are installed.
+BEGIN {
+    my $libpath;
+    if (! $ENV{MOGILE_NO_BUILTIN_DEPS} &&
+        ($libpath = $INC{"MogileFS/Server.pm"}) &&
+        $libpath =~ s!MogileFS/Server.pm$!!)
+    {
+        my $dep_dir = "${libpath}mogdeps";
+        push @INC, $dep_dir;
+        unless (($ENV{PERL5LIB} || "") =~ /$dep_dir/) {
+            $ENV{PERL5LIB} = join(":",
+                                  split(/:/, $ENV{PERL5LIB} || ""),
+                                  $dep_dir);
+        }
+    }
+}
+
 use IO::Socket;
 use Symbol;
 use POSIX;
