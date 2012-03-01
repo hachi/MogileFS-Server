hachi/MogileFS-Server

commit 6c32b4c1f4e3ee83e85e049e179d226790d2683d

Postgres wasn't honoring a no-wait timeout
passing a 0 timeout to get_lock means "try and give up immediately"

lib/MogileFS/Store/Postgres.pm
@@ -796,11 +796,11 @@ sub get_lock {
     debug("$$ Locking $lockname ($lockid)\n") if $Mgd::DEBUG >= 5;
 
     my $lock = undef;
-    while($timeout > 0 and not defined($lock)) {
+    while($timeout >= 0 and not defined($lock)) {
         $lock = eval { $self->dbh->do('INSERT INTO lock (lockid,hostname,pid,acquiredat) VALUES (?, ?, ?, '.$self->unix_timestamp().')', undef, $lockid, hostname, $$) };
         if($self->was_duplicate_error) {
             $timeout--;
-            sleep 1;
+            sleep 1 $timeout > 0;
             next;
         }
         $self->condthrow;

commit 4f9da04fd01e8f563ca6a521bbf7cc51be1e63fe

postgres: fix syntax error when getting lock
Introduced in commit 6c32b4c1f4e3ee83e85e049e179d226790d2683d

lib/MogileFS/Store/Postgres.pm
@@ -800,7 +800,7 @@ sub get_lock {
         $lock = eval { $self->dbh->do('INSERT INTO lock (lockid,hostname,pid,acquiredat) VALUES (?, ?, ?, '.$self->unix_timestamp().')', undef, $lockid, hostname, $$) };
         if($self->was_duplicate_error) {
             $timeout--;
-            sleep 1 $timeout > 0;
+            sleep 1 if $timeout > 0;
             next;
         }
         $self->condthrow;