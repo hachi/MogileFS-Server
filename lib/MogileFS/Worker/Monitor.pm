b0f05b75eabbfdb8114aa6369520defdaf614141
Eric Wong <normalperson@yhbt.net>
2014-02-01 08:49:10 +0000

    monitor: ping parent during deferred DB updates
    With enough devices and high enough network latency to the DB,
    we bump into the watchdog timeout of 30s easily.

--- lib/MogileFS/Worker/Monitor.pm
+++ lib/MogileFS/Worker/Monitor.pm
@@ -182,6 +182,7 @@ sub usage_refresh_done {
         my $updates = delete $self->{devs_to_update};
         foreach my $upd (@$updates) {
             $sto->update_device_usage(%$upd);
+            $self->still_alive;
         }
         $sto->release_lock('mgfs:device_update');
         $self->{updateable_devices} = undef;
